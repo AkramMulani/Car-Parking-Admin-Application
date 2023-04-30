c7ad3e89aa50aa33f1d8a2a35c44dbd00329238a
AkramMulani <101826471+AkramMulani@users.noreply.github.com>
2023-04-30 15:22:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+    ######### CAR PARKING ADMIN APPLICATION ##########
+    AUTHER :    AKRAM MULANI
+"""
+
+from Application import *
+from DBConnection import *
+
+app = App()
+app.__start__()
